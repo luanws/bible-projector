4577e68b26ee7d21851ef1a60ba6c2122ba98f03
luanws <luan.w.silveira@gmail.com>
2021-07-17 17:50:03 -0300

    remove comments .ui files

--- scripts/update_windows.py
+++ scripts/update_windows.py
@@ -1,6 +1,28 @@
 import os
+import re
+from typing import List, Tuple
 
 from scripts import Script
+
+
+def get_file_name_and_roots(directory: str, pattern: str) -> List[Tuple[str, str]]:
+    file_name_and_roots: List[Tuple[str, str]] = []
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            if re.match(pattern, filename):
+                file_name_and_roots.append((root, filename))
+    return file_name_and_roots
+
+
+def remove_comments_from_python_file(path: str):
+    with open(path) as file:
+        content = file.read()
+
+    content = re.sub(r'#.*', '', content)
+    content = re.sub(r'^\n+', '', content)
+
+    with open(path, 'w') as file:
+        file.write(content)
 
 
 class UpdateWindowsScript(Script):
@@ -8,13 +30,11 @@ class UpdateWindowsScript(Script):
         return 'atualizar janelas do PyQt'
 
     def __call__(self):
-        current_path = os.path.abspath(os.getcwd())
-
-        for package in os.listdir(os.path.join(current_path, 'src', 'ui')):
-            main_view_ui_path = os.path.join(
-                current_path, 'src', 'ui', package, 'window.ui')
-            main_view_py_path = os.path.join(
-                current_path, 'src', 'ui', package, 'window.py')
-            os.system(f"pyuic5 {main_view_ui_path} -o {main_view_py_path}")
+        for root, ui_file_name in get_file_name_and_roots('src', r'.+\.ui$'):
+            filename = os.path.splitext(ui_file_name)[0]
+            ui_file_path = os.path.join(root, ui_file_name)
+            py_file_path = os.path.join(root, f'{filename}.py')
+            os.system(f"pyuic5 {ui_file_path} -o {py_file_path}")
+            remove_comments_from_python_file(py_file_path)
 
         print('Janelas atualizadas com sucesso')
--- src/ui/projector/window.py
+++ src/ui/projector/window.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'E:\GitHub\bible-projector\python\src\ui\projector\window.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.1
-#
-# WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
