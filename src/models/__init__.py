2b852c160d320c1cf4440c48b1a3b1f7104a296c
Luan Willig Silveira <luan.w.silveira@gmail.com>
2021-07-17 14:11:43 -0300

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,5 @@
 from src.models.book import Book
-from src.models.version import Version
+from src.models.chapter_reference import ChapterReference
 from src.models.verse import Verse
+from src.models.verse_reference import VerseReference
+from src.models.version import Version
