acdc2c4a5015b2abe893f738988cf72649f3bb59
luanws <luan.w.silveira@gmail.com>
2021-07-17 15:34:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 
 import sqlalchemy.ext.baked
 import sqlalchemy.sql.default_comparator
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication
 
 from src.ui.main import MainWindow
@@ -9,5 +10,6 @@ from src.ui.main import MainWindow
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_window = MainWindow()
+    main_window.setWindowIcon(QtGui.QIcon('icon.ico'))
     main_window.showMaximized()
     app.exec()
