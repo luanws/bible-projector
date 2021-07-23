9e81537f27f5927e5a196c8086375c0301cf7f0f
luanws <luan.w.silveira@gmail.com>
2021-07-23 18:28:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from PyQt5.QtWidgets import QApplication
 
 from src.ui.main import MainWindow
 
+
 def main():
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.setWindowIcon(QtGui.QIcon('icon.ico'))
     main_window.showMaximized()
     app.exec()
+
 
 if __name__ == '__main__':
     main()
