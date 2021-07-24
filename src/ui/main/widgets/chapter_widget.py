26a3b1705c9504775cb21bcc48a3d6bb2e20f5b5
luanws <luan.w.silveira@gmail.com>
2021-07-24 02:26:10 -0300

    Update chapter_widget.py

--- src/ui/main/widgets/chapter_widget.py
+++ src/ui/main/widgets/chapter_widget.py
@@ -3,6 +3,11 @@ from src.models.verse import Verse
 
 
 class ChapterVerseWidget (QtWidgets.QWidget):
+    verse: Verse
+    list_widget_item: QtWidgets.QListWidgetItem
+    list_widget: QtWidgets.QListWidget
+    selected: bool
+
     def __init__(
         self, parent=None, *,
         verse: Verse,
@@ -13,6 +18,7 @@ class ChapterVerseWidget (QtWidgets.QWidget):
 
         self.list_widget_item = list_widget_item
         self.list_widget = list_widget
+        self.selected = False
 
         self.container = QtWidgets.QHBoxLayout()
         self.verse_label = QtWidgets.QLabel()
@@ -33,12 +39,16 @@ class ChapterVerseWidget (QtWidgets.QWidget):
         ''')
 
     def select(self):
-        self.list_widget.scrollToItem(self.list_widget_item)
-        self.verse_label.setStyleSheet('''
-            color: green;
-        ''')
+        if not self.selected:
+            self.list_widget.scrollToItem(self.list_widget_item)
+            self.verse_label.setStyleSheet('''
+                color: green;
+            ''')
+            self.selected = True
 
     def unselect(self):
-        self.verse_label.setStyleSheet('''
-            color: black;
-        ''')
+        if self.selected:
+            self.verse_label.setStyleSheet('''
+                color: black;
+            ''')
+            self.selected = False
