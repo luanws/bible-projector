f7e66bc7a2278006331cfa583dadcba4b46604f7
luanws <luan.w.silveira@gmail.com>
2021-09-25 02:19:25 -0300

    Update __init__.py

--- src/utils/settings/__init__.py
+++ src/utils/settings/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from abc import ABC
 from configparser import ConfigParser
 from contextlib import suppress
-from typing import Any, Dict
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 
 def filter_dict_not_starts_with_underscore(d: Dict[str, Any]) -> Dict[str, Any]:
@@ -12,12 +12,21 @@ def filter_dict_not_starts_with_underscore(d: Dict[str, Any]) -> Dict[str, Any]:
 
 class Settings(ABC):
     config: ConfigParser = ConfigParser()
+    settings_instances: Dict[str,
+                             List[Tuple[Settings, Optional[Callable]]]] = {}
 
-    def __init__(self) -> None:
+    def __init__(self, on_change_settings: Optional[Callable] = None) -> None:
         super().__init__()
+        self.__add_setting_instance(on_change_settings)
         self.__load_config()
         self.__load_config_by_defaults()
-        self.init_subclass()
+        self.__init_subclass()
+
+    def __add_setting_instance(self, on_change_settings: Optional[Callable]):
+        if not Settings.settings_instances.__contains__(self.__class__.__name__):
+            Settings.settings_instances[self.__class__.__name__] = []
+        Settings.settings_instances[self.__class__.__name__].append(
+            (self, on_change_settings))
 
     def __load_config(self):
         if not Settings.config.sections():
@@ -29,7 +38,7 @@ class Settings(ABC):
             config_dict = filter_dict_not_starts_with_underscore(cls.__dict__)
             Settings.config[cls.__name__] = config_dict
 
-    def init_subclass(self):
+    def __init_subclass(self):
         with suppress(KeyError):
             section = Settings.config[self.__class__.__name__]
             config_dict: Dict[str, Any] = dict(section)
@@ -37,6 +46,8 @@ class Settings(ABC):
 
     def save(self):
         Settings.config[self.__class__.__name__] = self.to_dict()
+        self.update_settings_instances()
+        self.notify_settings_instances()
         with open('config.ini', 'w') as file:
             Settings.config.write(file)
 
@@ -47,3 +58,13 @@ class Settings(ABC):
         for k, v in self.__annotations__.items():
             config_dict[k] = v(config_dict[k])
         self.__dict__ = config_dict
+
+    def update_settings_instances(self):
+        for setting_instance, _ in Settings.settings_instances[self.__class__.__name__]:
+            if setting_instance is not self:
+                setting_instance.from_dict(self.to_dict().copy())
+
+    def notify_settings_instances(self):
+        for _, on_change_settings in Settings.settings_instances[self.__class__.__name__]:
+            if on_change_settings is not None:
+                on_change_settings()
