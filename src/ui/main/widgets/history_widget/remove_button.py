bd9635beead0b56b3fe0aa29e9a0fbe4325b27ee
luanws <luan.w.silveira@gmail.com>
2021-10-02 02:16:12 -0300

    Update remove_button.py

--- src/ui/main/widgets/history_widget/remove_button.py
+++ src/ui/main/widgets/history_widget/remove_button.py
@@ -1,15 +1,24 @@
 import qtawesome as qta
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class RemoveButton(QtWidgets.QPushButton):
     def __init__(self, parent=None):
         super().__init__(parent)
-        
-        self.setIcon(qta.icon('fa5s.times', color='#EC0059'))
+
+        self.setIcon(qta.icon('fa5s.times', color='#AAA'))
         self.setIconSize(QtCore.QSize(20, 20))
         self.setMaximumSize(QtCore.QSize(32, 32))
+        self.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.setStyleSheet('''
-            border: none;
             background-color: transparent;
         ''')
+
+        self.installEventFilter(self)
+
+    def eventFilter(self, object: QtCore.QObject, event: QtCore.QEvent) -> bool:
+        if event.type() == QtCore.QEvent.HoverEnter:
+            self.setIcon(qta.icon('fa5s.times', color='#EC0059'))
+        elif event.type() == QtCore.QEvent.HoverLeave:
+            self.setIcon(qta.icon('fa5s.times', color='#AAA'))
+        return super().eventFilter(object, event)
