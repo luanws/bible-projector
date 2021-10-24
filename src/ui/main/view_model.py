bc36af81a167e3c3c802fe93811f43b96ea63079
luanws <luan.w.silveira@gmail.com>
2021-10-24 02:24:20 -0300

    Update view_model.py

--- src/ui/main/view_model.py
+++ src/ui/main/view_model.py
@@ -39,13 +39,14 @@ class MainViewModel:
         self.application: QCoreApplication = QCoreApplication.instance()
 
         self.history_list = []
-        self.versions = [v.version for v in self.version_dao.get_all()]
+        self.update_versions()
         self.current_version = self.versions[0]
         self.__current_verse: Optional[Verse] = None
         self.current_chapter: Optional[List[Verse]] = None
 
     def update_versions(self):
         self.versions = [v.version for v in self.version_dao.get_all()]
+        self.versions.sort()
 
     def on_change_current_verse(self, callable: Callable[[Verse], None]):
         self.__on_change_current_verse_callable = callable
