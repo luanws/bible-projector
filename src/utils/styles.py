df097b6fce10e2b10027b181c927f33745ee36d5
luanws <luan.w.silveira@gmail.com>
2021-11-06 14:29:04 -0300

    feat(styles): variáveis de qss

--- src/utils/styles.py
+++ src/utils/styles.py
@@ -1,18 +1,36 @@
 import json
 import os
+import re
 from typing import Dict
 
 qss_dict: Dict[str, str] = {}
+qss_vars: Dict[str, str] = {}
+
+
+def get_stylesheet_vars() -> Dict[str, str]:
+    file_path = os.path.join('data', 'styles', 'vars.txt')
+    with open(file_path) as f:
+        content = f.read()
+    groups = re.findall(r'(@[^\s]+)\s*=\s*([^\s]+)', content, re.MULTILINE)
+    return {group[0]: group[1] for group in groups}
+
+
+def apply_qss_vars(qss_vars: Dict[str, str], qss_stylesheet: str) -> str:
+    for key, value in qss_vars.items():
+        qss_stylesheet = qss_stylesheet.replace(key, value)
+    return qss_stylesheet
 
 
 def get_qss_dict() -> Dict[str, str]:
-    file_path = os.path.join('data', 'styles.json')
+    file_path = os.path.join('data', 'styles', 'stylesheets.json')
     with open(file_path) as f:
         return json.load(f)
 
 
 def update_qss_dict():
     global qss_dict
+    global qss_vars
+    qss_vars = get_stylesheet_vars()
     qss_dict = get_qss_dict()
 
 
@@ -23,4 +41,5 @@ def check_paths_is_equal(path1: str, path2: str) -> bool:
 def get_qss_stylesheet(qss_file_path) -> str:
     if len(qss_dict) == 0:
         update_qss_dict()
-    return qss_dict[qss_file_path]
+    stylesheet = qss_dict[qss_file_path]
+    return apply_qss_vars(qss_vars, stylesheet)
