485c5d75378e7e569cf257d93d0d2d9186bd187a
luanws <luan.w.silveira@gmail.com>
2021-11-06 22:39:17 -0300

    feat(splash): splash screen

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import sys
 
 import sqlalchemy.ext.baked
 import sqlalchemy.sql.default_comparator
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QSplashScreen
 
 from src.ui.main import MainWindow
 from src.utils import styles
@@ -10,8 +11,15 @@ from src.utils import styles
 
 def main():
     app = QApplication(sys.argv)
+
+    splash_screen = QSplashScreen(QPixmap('data/img/splash.png'))
+    splash_screen.show()
+
     main_window = MainWindow()
     main_window.showMaximized()
+
+    splash_screen.close()
+
     app.setStyleSheet(styles.get_qss_stylesheet('src/styles/global.qss'))
     app.exec()
 
