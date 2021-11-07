812cdcfd9247c586ae71a22b9210252a849adb1b
luanws <luan.w.silveira@gmail.com>
2021-11-06 22:13:20 -0300

    Update view_model.py

--- src/ui/advanced_search/view_model.py
+++ src/ui/advanced_search/view_model.py
@@ -1,35 +1,36 @@
-from typing import List, Optional
+from typing import List
 
 from src.dao.book_dao import BookDAO
 from src.dao.verse_dao import VerseDAO
 from src.dao.version_dao import VersionDAO
 from src.models.verse import Verse
-from src.models.version import Version
 
 
 class AdvancedSearchViewModel:
     verse_dao: VerseDAO
     version_dao: VersionDAO
     book_dao: BookDAO
-    __versions: List[Version]
+    __version_names: List[str]
+    __book_names: List[str]
 
     def __init__(self) -> None:
         self.verse_dao = VerseDAO()
         self.version_dao = VersionDAO()
         self.book_dao = BookDAO()
 
-        self.__versions = self.version_dao.get_all()
+        self.__version_names = [v.version for v in self.version_dao.get_all()]
+        self.__book_names = [b.name for b in self.book_dao.get_all()]
 
     def get_all_version_options(self) -> List[str]:
-        return ['Todas'] + [version.version for version in self.__versions]
+        return ['Todas'] + self.__version_names
 
     def get_all_book_options(self) -> List[str]:
-        return ['Qualquer livro'] + [book.name for book in self.book_dao.get_all()]
+        return ['Qualquer livro'] + self.__book_names
 
     def search(self, search_text: str, book: str = None,  version: str = None) -> List[Verse]:
-        if version == 'Todas':
+        if version not in self.__version_names:
             version = None
-        if book == 'Qualquer livro':
+        if book not in self.__book_names:
             book = None
         return self.verse_dao.filter(
             {'q': search_text, 'version': version, 'book': book},
