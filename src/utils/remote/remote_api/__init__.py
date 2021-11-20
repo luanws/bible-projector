0ae80f64adfda9e4701cc17d1cca4005ffa200dd
luanws <luan.w.silveira@gmail.com>
2021-11-20 18:41:25 -0300

    Update __init__.py

--- src/utils/remote/remote_api/__init__.py
+++ src/utils/remote/remote_api/__init__.py
@@ -11,17 +11,15 @@ class RemoteAPI(Remote):
     __server: Server
     __server_address: ServerAddress
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.__app = Flask(__name__)
-        self.__server_address = ServerAddress(port=5000)
-
     @property
     def address(self) -> str:
         return self.__server_address.address
 
     def start(self) -> None:
+        self.__app = Flask(__name__)
+        self.__server_address = ServerAddress(port=5000)
         self.__server_address.generate_prefix(40)
+        print(f'Server prefix: {self.__server_address.prefix}')
         return super().start()
 
     def _run(self) -> None:
@@ -31,15 +29,15 @@ class RemoteAPI(Remote):
         )
         self.configure_routes()
         print(f'Starting server on {self.__server_address.localhost_address}')
+        print(f'Starting server on {self.__server_address.address}')
         self.__server.run()
 
     def stop(self) -> None:
-        self.__server.shutdown()
+        self.__server.stop()
         print('Server stopped')
 
     def configure_routes(self) -> None:
         routes.execute = self.execute
-        self.__app.blueprints.clear()
         self.__app.register_blueprint(
             routes.api_routes_blueprint,
             url_prefix=f'/{self.__server_address.prefix}'
