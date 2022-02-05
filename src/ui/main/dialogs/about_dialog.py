bf27b0ac8320924d2a80859928b30c61179b8d00
luanws <luan.w.silveira@gmail.com>
2022-02-04 21:10:34 -0300

    refactor(version): version file renamed

--- src/ui/main/dialogs/about_dialog.py
+++ src/ui/main/dialogs/about_dialog.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QDialog, QLabel, QVBoxLayout
-from src import version
+from src.version import version
 
 
 class AboutDialog(QDialog):
